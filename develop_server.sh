4b223a07c78ce272acb7cb4dfb8d6c078f1ea391
wuguohua <wuguohua@bytedance.com>
2016-04-18 23:07:43 +0800

    ignore pid file

--- develop_server.sh
+++ develop_server.sh
@@ -67,7 +67,7 @@ function start_up(){
   pelican_pid=$!
   echo $pelican_pid > $PELICAN_PID
   cd $OUTPUTDIR
-  $PY -m pelican.server $port 0.0.0.0 &
+  $PY -m pelican.server $port 127.0.0.1 &
   srv_pid=$!
   echo $srv_pid > $SRV_PID
   cd $BASEDIR
